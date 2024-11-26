e9d35aba4a8477ebfe1fa533f3836ed06204c387
marialeon-sils <melon@ad.unc.edu>
2024-11-25 23:49:33 -0500

    bmi rounded to 2 decimals

--- roster.py
+++ roster.py
@@ -6,5 +6,15 @@ player = {"Last Name": ["Claude", "Brown", "Cadeau", "Davis", "Tyson", "Davis", 
           "height": [79, 82, 73, 72, 79, 75, 75, 78, 76, 82],
           "weight": [230, 215, 180, 180, 200, 215, 195, 195, 190, 235]
           }
+
 data = pd.DataFrame(player)
+
+# bmi = weight in kg/ height in meters squared
+data["bmi"] = (data["weight"]/2.205)/((data["height"]/39.37)**2)
+
+# round bmi to 2 decimal places
+data["bmi"] = data["bmi"].round(2)
+
 print(data)
+
+data.to_csv("bmi.csv")
