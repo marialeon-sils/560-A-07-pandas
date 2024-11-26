f01294a1f73630924946ca9b051f92924817b1e6
marialeon-sils <melon@ad.unc.edu>
2024-11-25 23:42:08 -0500

    Remove Roster Bar

--- roster.py
+++ roster.py
@@ -1,10 +1,10 @@
 # https://goheels.com/sports/mens-basketball/roster
 import pandas as pd
 
-roster = ["Claude", "Brown", "Cadeau", "Davis", "Tyson", "Davis", "Trimble", "Powell", "Jackson", "Washington" ]
-player = {"Last Name": roster,
+player = {"Last Name": ["Claude", "Brown", "Cadeau", "Davis", "Tyson", "Davis", "Trimble", "Powell", "Jackson", "Washington"],
           "First Name": ["Ty", "James", "Elliot", "RJ", "Cade", "Elijah", "Seth", "Drake", "Ian", "Jalen"],
           "height": [79, 82, 73, 72, 79, 75, 75, 78, 76, 82],
-          "weight": [230, 215, 180, 180, 200, 215, 195, 195, 190, 235]}
+          "weight": [230, 215, 180, 180, 200, 215, 195, 195, 190, 235]
+          }
 data = pd.DataFrame(player)
 print(data)
